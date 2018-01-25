116268a0bd64f5d74f978d39566584ffd4f85b7d
PROSPER KELLEY <prokelle@uat.edu>
2018-01-25 12:56:55 -0700

    Add project files.

---
+++ MyyNameGuessMyNumber/MyyNameGuessMyNumber.cpp
@@ -0,0 +1,18 @@
+// MyyNameGuessMyNumber.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+
+using namespace std;
+
+
+
+int main()
+{
+	srand(static_cast<unsigned)
+    return 0;
+}
+
