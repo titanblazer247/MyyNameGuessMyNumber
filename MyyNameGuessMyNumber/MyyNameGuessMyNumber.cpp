b156ad0a1f9c85f2d632ee9369c105f7e1a2080f
PROSPER KELLEY <PROKELLE@uat.edu>
2018-01-25 13:07:24 -0700

    hi

--- MyyNameGuessMyNumber/MyyNameGuessMyNumber.cpp
+++ MyyNameGuessMyNumber/MyyNameGuessMyNumber.cpp
@@ -12,7 +12,32 @@ using namespace std;
 
 int main()
 {
-	srand(static_cast<unsigned)
+	srand(static_cast<unsigned int>(time(0)));
+
+	int secretnumber = rand() % 100 + 1; // random number between 1 and 100
+	int tries = 0;
+		int guess;
+		
+		{
+
+		
+		cout << "\welcome to guess my number\n\n";
+		cout << "Enter a guess:";
+		cin >> guess;
+		++tries;
+		
+		if (guess > secretnumber)
+		{
+			cout << "too high!\n\n";
+		}
+		else if (guess < secretnumber)
+		{
+			cout << "too low!\n\n";
+		}
+		else
+		{
+			cout << "\nthat's it! you got it in" << tries << "guesses!\n";
+		}
     return 0;
 }
 
